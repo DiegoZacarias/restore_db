5e9a80d5211555dda648681f64274dd6ec284eb3
DiegoZacarias <diegozaca97@gmail.com>
2023-03-06 19:51:44 -0300

    add config file

--- main.py
+++ main.py
@@ -4,49 +4,57 @@ import os, subprocess, time
 import docker
 import colorama
 from colorama import Fore
-# from configparser import ConfigParser
 import configparser
 import time
 
-projectDir = '/Users/diego/sites/talently/l9-ops-backend-api'
-dockerStorageDir = '/Users/diego/sites/talently/l9-ops-backend-api/docker/storage/databases_backup'
+parser = configparser.ConfigParser()
+parser.read("configfile.ini")
+client = docker.from_env()
+container = client.containers.get(f'{parser.get("general", "container_name")}')
 
+database_name = parser.get('general', 'database_name')
 
-def main():
-    st = time.time()
-    # docker exec -it leacy php artisan tinker
-    client = docker.from_env()
-    # container = client.containers.get('leacy')
-    container = client.containers.get('mysql')
+def validate_container():
+    # check if container exists
+    if not container:
+        print(Fore.RED + 'Container does not exist')
+        exit()
 
-    # container.logs()
-    print(Fore.WHITE + f'Short id: {container.short_id}')
-    print(Fore.GREEN + f'Estado: {container.status}')
-    print(Fore.GREEN + f'Image: {container.image}')
-
+def validate_container_status():
     # if container.status == 'exited' then print an error message and exit
     if container.status == 'exited':
         print(Fore.RED + 'Container is not running')
         exit()
 
-
-
-    print(Fore.WHITE + 'Verificando existencia de backup...')
-    if not os.path.isfile(dockerStorageDir + '/backup.sql'):
+def validate_backup_file():
+    # check if backup file exists
+    if not os.path.isfile(parser.get('general', 'docker_storage_dir') + '/backup.sql'):
         print(Fore.RED + 'No se encontró el archivo de backup, asegurese de haberlo generado')
         exit()
 
+def restore_database():
+    # create .cnf file for mysql with the user and password to connect to the database, check: https://stackoverflow.com/questions/62287061/connect-to-2-instances-of-mysql-with-no-password-interaction-using-command-line
+    mysql_command = f"drop database if exists {database_name}; create database {database_name}; use {database_name}; source backup.sql;"
+    result = container.exec_run(f'mysql -e"{mysql_command}"', workdir='/home/database_backup/', user='root', stdin=False)
+
+def main():
+    st = time.time()
+
+    print(Fore.WHITE + f'Short id: {container.short_id}')
+    print(Fore.GREEN + f'Estado: {container.status}')
+    print(Fore.GREEN + f'Image: {container.image}')
+
+    validate_container()
+    validate_container_status()
+    validate_backup_file()
 
     print(Fore.WHITE + 'Backup encontrado, restaurando...')
 
-    # create .cnf file for mysql with the user and password to connect to the database, check: https://stackoverflow.com/questions/62287061/connect-to-2-instances-of-mysql-with-no-password-interaction-using-command-line
-
-    mysql_command = "drop database if exists leacy; create database leacy; use leacy; source backup.sql;"
-    result = container.exec_run(f'mysql -e"{mysql_command}"', workdir='/home/database_backup/', user='root', stdin=False)
+    restore_database()
 
     et = time.time()
     elapsed_time = et - st
-    # Aprox 1000 segundos, 16 minutos en pruebas
+    # Aprox 1000 segundos, 16 minutos en pruebas para db de talently
     print(Fore.YELLOW + 'Execution time:', elapsed_time, 'seconds')
     print(Fore.GREEN + 'Restauración completada')
 
